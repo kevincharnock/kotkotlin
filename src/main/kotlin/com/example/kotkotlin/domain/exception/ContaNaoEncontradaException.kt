4574a0c2c64961686abfba5b8e41668b3ed1b260
Kevin Charnock <kevinshuldiner@gmail.com>
2026-07-06 15:50:56 -0300

    First Commit

--- src/main/kotlin/com/example/kotkotlin/domain/exception/ContaNaoEncontradaException.kt
+++
@@ -1,5 +0,0 @@
-package com.example.kotkotlin.domain.exception
-
-
-class ContaNaoEncontradaException(numero: String) :
-    RuntimeException("Conta $numero não encontrada")
--- src/main/kotlin/com/example/kotkotlin/domain/exception/ValorInvalidoException.kt
+++
@@ -1,3 +0,0 @@
-package com.example.kotkotlin.domain.exception
-
-class ValorInvalidoException(mensagem: String) : RuntimeException(mensagem)
